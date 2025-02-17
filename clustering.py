1c4824c97a69c5bc5fadc064de08468b94b4f9bf
simvers <simon.verstraeten@kuleuven.be>
2025-02-17 14:15:57 +0100

    Preprocessing and clustering

---
+++ clustering.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+
+# Import clean data
+data = pd.read_excel('data/data_clean.xlsx')
+composition = pd.read_excel('data/composition_clean.xlsx')
+print(data.head(10))
+
+# Cluster data
+n_cluster = np.arange(1, 16, 1)
+k_score = []
+for i in n_cluster:
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(composition)
+    k_score.append(kmeans.score(composition))
+
+# Plot
+plt.plot(n_cluster, np.gradient(k_score), 'DarkBlue')
+plt.xlabel('Number of clusters')
+plt.ylabel('K-means score')
+plt.show()
+
+# Optimal clustering
+n_cluster = 3
+opt_cluster = KMeans(n_clusters=n_cluster)
+clustering = opt_cluster.fit_predict(composition)
+print(clustering)
+
+# Centroids
+centroids = pd.DataFrame(opt_cluster.cluster_centers_, columns=composition.columns)
+centroids = centroids.apply(lambda row: row.sort_values(ascending=False).index.values[:5], axis=1)
+print(centroids)
+# Cluster 1: [P, V, Ti, Si, W]
+# Cluster 2: [Si, Al, Cs, P, Na]
+# Clsuter 3: [Al, Cs, Ti, P, Ba]
+
+# PCA
+pca = PCA(n_components=2)
+pca_reduced_data = pca.fit_transform(composition)
+plt.scatter(pca_reduced_data[:, 0], pca_reduced_data[:, 1], c=clustering, cmap=plt.get_cmap('plasma', n_cluster), label=centroids)
+plt.show()
+
+# TSNE
+tsne = TSNE(n_components=2, init='pca')
+tsne_reduced_data = tsne.fit_transform(composition)
+plt.scatter(tsne_reduced_data[:, 0], tsne_reduced_data[:, 1], c=clustering, cmap=plt.get_cmap('plasma', n_cluster), label=centroids)
+plt.legend()
+plt.show()
---
+++ preprocess.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import matplotlib.pyplot as plt
+
+
+# Load data and extract columns
+data = pd.read_excel(r"C:\Users\u0156112\OneDrive - KU Leuven\Shared_AC2GEN\Review\Review_V001\Catalysts.xlsx")
+print(data.columns)
+get_columns = ['Atom 1', 'Atom 2', 'Atom 3', 'Atom 4', 'Atom 5', 'Atom 6',
+               'LHSV [ml/h/g]', 'Temperature [K]', 'STY MA+AA (mmol/h/g)']
+data = data[get_columns]
+
+# Encode atom composition
+atom_columns = ['Atom 1', 'Atom 2', 'Atom 3', 'Atom 4', 'Atom 5', 'Atom 6']
+data['Dummy'] = np.ones(len(data))
+pivoted_tables = [data.pivot_table(columns=column_i, index=data.index, values='Dummy', fill_value=0) for column_i in atom_columns]
+composition_clean = pd.concat(pivoted_tables, axis=1, sort=True).fillna(0).T.groupby(level=0).sum().T
+print(composition_clean.columns)
+
+# Concat encoded atom composition and rest of data
+data_clean = pd.concat(
+    [composition_clean, 
+     data[[column for column in get_columns if column not in atom_columns]]], 
+    axis=1
+)
+print(data_clean.head(10))
+
+# Save clean data
+composition_clean.to_excel('data/composition_clean.xlsx', index=False)
+data_clean.to_excel('data/data_clean.xlsx', index=False)
+
+# Most popular elements
+major_elements = composition_clean.sum(axis=0).sort_values(ascending=False)
+print(major_elements)
