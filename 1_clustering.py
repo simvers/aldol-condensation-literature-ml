b50f6ba29f49ec76a316c5741c6ee7948223f7e3
simvers <139957195+simvers@users.noreply.github.com>
2025-10-10 12:49:54 +0200

    Clean clustering

--- 1_clustering.py
+++ 1_clustering.py
@@ -6,25 +6,25 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
 
-# Import clean data
+# Import processed data and elements
 data = pd.read_csv('data/data_processed.csv', na_values=[''], keep_default_na=False)
 elements = pd.read_csv('data/elements.csv', header=None).squeeze('columns').to_list()
 composition = data.loc[:, elements]
-print(data.head(10), '\n', composition.head(10))
 assert composition.notna().all(axis=None)
 
-# Dummy composition
-# composition[:] = np.where(composition < 1e-10, 0, 1)
-print(composition)
+# Binarize composition
+binarize = True
+if binarize:
+    composition[:] = np.where(composition < 1e-10, 0, 1)
 
 # Control randomness
 np.random.seed(4321)
 
 # Cluster data
-
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
 for _ in range(10):
         
-    n_cluster = np.arange(1, 16, 1)
+    n_cluster = np.arange(1, 10, 1)
     k_score = []
     for i in n_cluster:
         kmeans = KMeans(n_clusters=i, n_init=20)
@@ -32,17 +32,14 @@ for _ in range(10):
         k_score.append(kmeans.score(composition))
 
     # Plot
-    plt.plot(n_cluster, np.gradient(k_score), 'DarkBlue')
-    plt.xlabel('Number of clusters')
-    plt.ylabel('K-means score')
-
-plt.show()
+    ax.plot(n_cluster, np.gradient(k_score), 'DarkBlue')
+ax.set(xlabel='Number of clusters', ylabel='K-means score', xlim=(1, 9), xticks=[1, 3, 5, 7, 9], yticks=[])
+fig.savefig(f'figures/clustering/clustering_score{"_binarized"*binarize}.png', dpi=300)
 
 # Optimal clustering
-n_cluster = 4
+n_cluster = 3 if binarize else 4
 opt_cluster = KMeans(n_clusters=n_cluster)
 clustering = opt_cluster.fit_predict(composition)
-# print(clustering)
 
 # Centroids
 centroids = pd.DataFrame(opt_cluster.cluster_centers_, columns=composition.columns)
@@ -55,11 +52,13 @@ print(centroids)
 # Cluster 2: [Si, Al, Cs, P, Na]
 # Cluster 3: [Al, Cs, Ti, P, Ba]
 
+# --------------------------------------
+
 # Save clusters
+
 # Concat cluster number
-data_clustered = pd.concat(
-    [data, pd.DataFrame(clustering, columns=['Cluster_n'])], axis=1
-)
+data_clustered = pd.concat([data, pd.DataFrame(clustering, columns=['Cluster_n'])], axis=1)
+
 # Concat cluster title
 for i in range(n_cluster):
     data_clustered.loc[data_clustered['Cluster_n'] == i, 'Cluster_title'] = centroids.loc[i, 'Cluster_title']
